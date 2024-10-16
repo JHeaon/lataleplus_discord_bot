cae084a52f81e58170a688ed278443f29edbdd1c
JHeaon <j3heawon@gmail.com>
2024-10-16 16:19:41 +0900

    Feat: 크롤링 주기 변경

--- main.py
+++ main.py
@@ -7,40 +7,43 @@ from handle.handler import Handler
 from notify.discord_notifier import DiscordNotifier
 from observer.monitor import Monitor
 
-
 if __name__ == "__main__":
 
+    def schedule_time(hour, minute):
+        return {'hours': hour, "minutes": minute}
+
+
     monitor_list = [
-                    Monitor(Crawler(os.environ.get("ANNOUNCEMENT_API")),
-                            Handler(os.environ.get("DB"), "Announcement"),
-                            DiscordNotifier(os.environ.get("DISCORD_ANNOUNCEMENT_API"))),
+        Monitor(Crawler(os.environ.get("ANNOUNCEMENT_API")),
+                 Handler(os.environ.get("DB"), "Announcement"),
+                 DiscordNotifier(os.environ.get("DISCORD_ANNOUNCEMENT_API"))),
 
-                    Monitor(Crawler(os.environ.get("INSPECTION_UPDATE_API")),
-                            Handler(os.environ.get("DB"), "InspectionUpdate"),
-                            DiscordNotifier(os.environ.get("DISCORD_INSPECTION_UPDATE_API"))),
+        Monitor(Crawler(os.environ.get("INSPECTION_UPDATE_API")),
+                Handler(os.environ.get("DB"), "InspectionUpdate"),
+                DiscordNotifier(os.environ.get("DISCORD_INSPECTION_UPDATE_API"))),
 
-                    Monitor(Crawler(os.environ.get("DEVELOPER_NOTE_API")),
-                            Handler(os.environ.get("DB"), "DeveloperNote"),
-                            DiscordNotifier(os.environ.get("DISCORD_DEVELOPER_NOTE_API"))),
+        Monitor(Crawler(os.environ.get("DEVELOPER_NOTE_API")),
+                Handler(os.environ.get("DB"), "DeveloperNote"),
+                DiscordNotifier(os.environ.get("DISCORD_DEVELOPER_NOTE_API"))),
 
-                    Monitor(Crawler(os.environ.get("EVENT_API")),
-                            Handler(os.environ.get("DB"), "Event"),
-                            DiscordNotifier(os.environ.get("DISCORD_EVENT_API"))),
+        Monitor(Crawler(os.environ.get("EVENT_API")),
+                Handler(os.environ.get("DB"), "Event"),
+                DiscordNotifier(os.environ.get("DISCORD_EVENT_API"))),
 
-                    Monitor(Crawler(os.environ.get("EVENT_WINNER_API")),
-                            Handler(os.environ.get("DB"), "EventWinner"),
-                            DiscordNotifier(os.environ.get("DISCORD_EVENT_WINNER_API"))),
+        Monitor(Crawler(os.environ.get("EVENT_WINNER_API")),
+                Handler(os.environ.get("DB"), "EventWinner"),
+                DiscordNotifier(os.environ.get("DISCORD_EVENT_WINNER_API"))),
 
-                    Monitor(Crawler(os.environ.get("FREE_BULLET_IN_BOARD")),
-                            Handler(os.environ.get("DB"), "FreeBulletinBoard"),
-                            DiscordNotifier(os.environ.get("DISCORD_FREE_BULLET_IN_BOARD_API"))),
+        Monitor(Crawler(os.environ.get("FREE_BULLET_IN_BOARD")),
+                Handler(os.environ.get("DB"), "FreeBulletinBoard"),
+                DiscordNotifier(os.environ.get("DISCORD_FREE_BULLET_IN_BOARD_API"))),
 
-                    ]
+    ]
 
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
 
     for monitor in monitor_list:
-        scheduler.add_job(monitor.run, "interval", seconds=3)
+        scheduler.add_job(monitor.run, "interval", minutes=1)
 
     scheduler.start()
 
